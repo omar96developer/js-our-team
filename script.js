e2ab76cd74227697d54b325214a84e669c6db5a5
omar96developer <omakino@hotmail.it>
2021-10-21 20:26:12 +0200

    finish

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 //OBJET WITH IMAGE NAME AND ROLE
 const membro = {
-    'image' : ['img/angela-caroll-chief-editor.jpg', 'img/walter-gordon-office-manager.jpg', 'img/angela-lopez-social-media-manager.jpg', 'img/scott-estrada-developer.jpg', 'img/barbara-ramos-graphic-designer.jpg'],
+    'image' : ['angela-caroll-chief-editor.jpg', 'walter-gordon-office-manager.jpg', 'angela-lopez-social-media-manager.jpg', 'scott-estrada-developer.jpg', 'barbara-ramos-graphic-designer.jpg'],
     'name' : ['Angela Caroll', 'Walter Gordon', 'Angela Lopez', 'Scott Estrada', 'Barbara Ramos'],
     'role' : ['Chief Editor', 'Office Manager', 'Social Media Manager', 'Developer', 'Graphic Designer'],
     
 };
-console.log(membro.image, membro.name, membro.role)
-console.log(membro.image[0])
+
+console.log(membro.name)
+console.log(membro.role)
+console.log(membro.image)
+
 
 //SELECTED DIV.TEAMCONTAINER
 const teamContainer = document.querySelector('.team-container');
 
-
-
 // LOOP FOR CREATE 5 TEAM CARD
-for(let i = 0; i < 5; i++){
+for(let i = 0; i < membro.name.length; i++){
     teamContainer.innerHTML += `<div class="team-card">
                                 <div class="card-image">
                                 <img
-                                    src=${membro.image[i]}
-                                    alt="Wayne Barnett"
+                                    src=img/${membro.image[i]}
+                                    alt="${membro.image[i]}"
                                 />
                                 </div>
                                 <div class="card-text">
@@ -27,4 +28,36 @@ for(let i = 0; i < 5; i++){
                                 <p>${membro.role[i]}</p>
                             </div>`;
 }
+
+
+
+const memberIpt = document.getElementById('addMemberButton');
+
+
+memberIpt.addEventListener('click', () => {
+    const nameIpt = document.getElementById('name').value;
+    console.log(nameIpt)
+    membro.name.push(nameIpt)
+
+    const roleIpt = document.getElementById('role').value;
+    console.log(roleIpt)
+    membro.role.push(roleIpt)
+
+    const imageIpt = document.getElementById('image').value;
+    console.log(imageIpt)
+    membro.image.push(imageIpt)
+
+    teamContainer.innerHTML += `<div class="team-card">
+                                <div class="card-image">
+                                <img
+                                    src=img/${imageIpt}
+                                    alt="${nameIpt}"
+                                />
+                                </div>
+                                <div class="card-text">
+                                <h3>${nameIpt}</h3>
+                                <p>${roleIpt}</p>
+                            </div>`;    
+
+})
 
